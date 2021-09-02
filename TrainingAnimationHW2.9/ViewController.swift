e9bae02d6bc9df475b98044342a41172f5ab8163
Seneca <flabbergasted242@gmail.com>
2021-09-02 12:22:31 +0300

    Initial Commit

---
+++ TrainingAnimationHW2.9/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrainingAnimationHW2.9
+//
+//  Created by Дмитрий Дмитрий on 02.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
